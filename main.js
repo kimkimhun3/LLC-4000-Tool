b52c301c2de502ecf55a54938a88103ecb0d9f09
Rin Kimhun <47348954+kimkimhun3@users.noreply.github.com>
2024-11-11 13:29:40 +0900

    Done Ver 01.03

--- main.js
+++ main.js
@@ -12,7 +12,10 @@ const createWindow = () => {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
+      //webSecurity: false,
+      //devTools: false // Disable developer tools
     },
+    //autoHideMenuBar: true
   });
 
   win.loadFile('index.html');
@@ -34,18 +37,52 @@ app.on('window-all-closed', () => {
   }
 });
 
+// ipcMain.on('open-file-dialog', (event) => {
+//   dialog.showOpenDialog(win, {
+//     properties: ['openFile'],
+//     filters: [{ name: 'Text Files', extensions: ['txt'] }],
+//   }).then((file) => {
+//     if (!file.canceled && file.filePaths.length > 0) {
+//       const content = fs.readFileSync(file.filePaths[0], 'utf-8');
+//       event.reply('file-path', file.filePaths[0]);
+//       event.reply('file-content', content);
+//     }
+//   });
+// });
+
+const path = require('path');
+
 ipcMain.on('open-file-dialog', (event) => {
   dialog.showOpenDialog(win, {
     properties: ['openFile'],
     filters: [{ name: 'Text Files', extensions: ['txt'] }],
   }).then((file) => {
     if (!file.canceled && file.filePaths.length > 0) {
-      const content = fs.readFileSync(file.filePaths[0], 'utf-8');
-      event.reply('file-path', file.filePaths[0]);
+      const inputFilePath = file.filePaths[0];
+      const inputDir = path.dirname(inputFilePath);
+      const baseName = path.basename(inputFilePath, '.txt');
+
+      const content = fs.readFileSync(inputFilePath, 'utf-8');
+      event.reply('file-path', inputFilePath);
       event.reply('file-content', content);
+
+      // Automatically create the four files in the same directory
+      const filesToCreate = [
+        { suffix: '_1.txt', eventName: 'file-created' },
+        { suffix: '_2.txt', eventName: 'file-created-2' },
+        { suffix: '_3.txt', eventName: 'file-created-3' },
+        { suffix: '_4.xlsx', eventName: 'file-created-4' },
+      ];
+
+      filesToCreate.forEach(({ suffix, eventName }) => {
+        const newFilePath = path.join(inputDir, `${baseName}${suffix}`);
+        fs.writeFileSync(newFilePath, '', 'utf-8'); // Creates an empty file
+        event.reply(eventName, newFilePath);
+      });
     }
   });
 });
+
 
 ipcMain.on('open-save-dialog', (event) => {
   dialog.showSaveDialog(win, {
